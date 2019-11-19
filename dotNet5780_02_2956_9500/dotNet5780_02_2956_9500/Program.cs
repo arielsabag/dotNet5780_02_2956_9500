d5b5ee9cb3051e7d948bc44c162e260237d96849
arielsabag <arielsabagsabag@gmail.com>
2019-11-19 19:25:38 +0200

    Just created project number 2

---
+++ dotNet5780_02_2956_9500/dotNet5780_02_2956_9500/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5780_02_2956_9500
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ariel sabag");
+        }
+    }
+}
