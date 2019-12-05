72649dd275aec3e02a03016a92474d7de008e33a
arielsabag <44552742+arielsabag@users.noreply.github.com>
2019-12-05 18:56:25 +0200

    Update HostingUnit.cs

--- dotNet5780_02_2956_9500/dotNet5780_02_2956_9500/HostingUnit.cs
+++ dotNet5780_02_2956_9500/dotNet5780_02_2956_9500/HostingUnit.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -47,187 +47,117 @@ namespace dotNet5780_02_2956_9500
         /// <returns>the unit serial number and The list of periods in which it is occupied. ( First Date and End for each Period) </returns>
         public override string ToString()
         {
-            return "Hosting unit key: " + hostingUnitKey + "\n" + printSchedule(Diary);
+            return "Hosting unit key: " + hostingUnitKey + "\n" +"" /*printSchedule(Diary)*/;
         }
 
 
-         /// <param name="Calendar">Calendar is the data base in which the hosting scedule saved inside</param>
 
         /// <summary>
         /// Accepts hosting requirement. If the matrix has a sequence of 
         /// Free days in accordance with the above requirement, the function indicates in the matrix that the unit was captured, 
         /// And signifies within the request itself that the request was approved(IsApproved = true)
         /// </summary>
-        /// <param name="guestReq"></param>
+        /// <param name="guestReq">It is a class that represents a guest request that contains 3 fields: 1- entry date, 2- relese date, 3- is approved(if the request approved bool type)</param>
         /// <returns> Returns true if the matrix has a sequence of Free days in accordance with the above requirement,
         /// If the requested dates are not available(even in part), returns "false". </returns>
         public bool ApprovedRequest(GuestRequest guestReq)
         {
-            bool outOfRange = false, occupied = false; 
-            int month, day, length;
-            //List<bool> diaryArray = new List<bool>();
-            int index = 1;
-            //diaryArray.Add(false);
-            bool[] diaryArray = new bool[31*12+1];
-
-            for (int i = 0; i < 31 * 12 + 1; i++)
+            bool[] diaryArray = new bool[31 * 12 + 1];// 
+            // coping the diary array to array of one dimension
+            for (int i = 0, index = 1; i < 12; i++)
             {
-
-                diaryArray[index] = false;
-                
-            }
-
-            for (int i = 0; i < 12; i++)
-            {
-                for (int j = 0; j < 31; j++)
+                for (int j = 0; j < 31; j++, index++)
                 {
                     diaryArray[index] = Diary[i, j];
-                    index++;
                 }
             }
 
+            bool outOfRange = false, occupied = false; // out of range - indicates if the dates requests is not in that year, occupied - if the date requests is not available
+            int month, day, length, tmpDay, tmpMonth;// month-entrydate.month, day - entrydate.day, length - releasedate - entrydate, tmpDay = day, tmpMonth = moth
             day = guestReq.EntryDate.Day;
             month = guestReq.EntryDate.Month;
             length = (guestReq.ReleaseDate - guestReq.EntryDate).Days;
-            int tmpDay = day, tmpMonth = month;
+            tmpDay = day;
+            tmpMonth = month;
 
-
-            if ((1 == length) && (Diary[month - 1, day - 1]))
+            // If length ==1
+            if (1 == length)
             {
+                // if it will be out of range
                 if (((((tmpMonth - 1) * 31) + tmpDay) + 1) == (31 * 12 + 1))
                 {
                     return false;
                 }
-                if (diaryArray.ElementAt((((month - 1) * 31) + day) + 1))
+                if (diaryArray.ElementAt((((month - 1) * 31) + day) + 1))// if dates not available
                 {
                     occupied = true;
                 }
             }
             else
             {
-                for (int i = 0; i < length; i++,tmpDay++) // iterate on all days and check if available
+                for (int i = 0; i < length; i++, tmpDay++) // iterate on all days and check if available
                 {
-                    if (((((tmpMonth - 1) * 31) + tmpDay) + 1)==(31*12+1))
+                    if (((((tmpMonth - 1) * 31) + tmpDay) + 1) == (31 * 12 + 1))// if out of range
                     {
                         outOfRange = true;
                         break;
                     }
-                    if (diaryArray.ElementAt((((tmpMonth - 1) * 31) + tmpDay) + 1))
+                    if (diaryArray.ElementAt((((tmpMonth - 1) * 31) + tmpDay) + 1))// if date not available
                     {
                         occupied = true;
                     }
                 }
             }
-            tmpDay = day;
-            tmpMonth = month;
+            tmpDay = day;// initialize tmpDay again to day beacouse we maby it has changed in the previous loop
+            tmpMonth = month;// initialize tmpMonth again to day beacouse we maby it has changed in the previous loop
 
-            if (outOfRange)
+            if ((outOfRange) || (occupied))// if the dates indeed out of range, or if the dates not available
             {
-                return false; // out of range
+                return false; // out of range - // the request was not accepted
             }
-            else
+            else// Here it means that the dates are not out of range  and available and  it will mark as true all dated from the requests
             {
-                if (occupied)
+                if (1 == length) 
                 {
-                    return false;
-                    // Console.WriteLine("the request was denied");
+                    diaryArray[((((month - 1) * 31) + day) + 1)] = true;
                 }
                 else
                 {
-                    if (1 == length)
+                    for (int i = 0; i < length; i++, tmpDay++) 
                     {
-                        diaryArray[((((month - 1) * 31) + day) + 1)] = true;
-                    }
-                    else
-                    {
-                        for (int i = 0; i < length; i++, tmpDay++) // iterate on all days and check if available
-                        {
-                            diaryArray[((((tmpMonth - 1) * 31) + tmpDay) + 1)] = true;
-                        }
+                        diaryArray[((((tmpMonth - 1) * 31) + tmpDay) + 1)] = true;
                     }
                 }
+                guestReq.IsApproved = true;// marks the bool field 'isApproved' ofguestRequest as 'true'
+                return true; // the request accepted
             }
-            guestReq.IsApproved = true;
-            return true;
+           
         }
 
-
-
-
         /// <summary>
-        ///  The function prints out for each hosting period it's first and last dayof hosting.
+        /// The function calulates out the number of occupied days out of the whole year.
         /// </summary>
-        /// <param name="Calendar">Calendaris the data base in which the hosting scedule saved inside</param>
-        private static string printSchedule(bool[,] Calendar)
-        {
-            string toReturn = string.Empty;
-            ArrayList tmpArr = new ArrayList();
-            for (int i = 0; i < 12; i++)
-            {
-                for (int j = 0; j < 31; j++)
-                {
-                    if (Calendar[i, j])
-                    {
-                        tmpArr.Add((j + 1) + "/" + (i + 1));
-                    }
-                    else
-                    {
-                        tmpArr.Add("empty");
-                    }
-                }
-            }
-            int state = 0;
-            for (int i = 0; i < tmpArr.Count; i++)
-            {
-                if ((i == 0) && (tmpArr[i].ToString() != "empty"))
-                {
-                    toReturn += tmpArr[i].ToString() + " - ";
-                    // Console.Write(tmpArr[i].ToString() + " - ");
-                    state++;
-                }
-                else if ((tmpArr[i].ToString() != "empty") && (state % 2 == 0))
-                {
-                    toReturn += tmpArr[i].ToString() + " - ";
-                    //Console.Write(tmpArr[i].ToString() + " - ");
-                    state++;
-                }
-                else if ((tmpArr[i].ToString() == "empty") && (state % 2 == 1))
-                {
-                    state++;
-                    toReturn += tmpArr[i - 1].ToString();
-                    //Console.WriteLine(tmpArr[i - 1].ToString());
-                }
-            }
-            return toReturn;
-        }
-
-
-
-        /// <summary>
-        /// The function prints out the number of occupied days out of the whole year, and it's percentage.
-        /// </summary>
-        /// <param name="Calendar">Calendaris the data base in which the hosting scedule saved inside</param>
+        /// <returns>The number of occupied days out of the whole year.</returns>
         public int GetAnnualBusyDays()
         {
-            int countOccpiedDays = 0;
+            int countOccupiedDays = 0;
             for (int i = 0; i < 12; i++)
             {
                 for (int j = 0; j < 31; j++)
                 {
                     if (Diary[i, j])
                     {
-                        countOccpiedDays++;
+                        countOccupiedDays++;
                     }
                 }
             }
-            return countOccpiedDays;
-
+            return countOccupiedDays;
         }
 
         /// <summary>
-        /// The function prints out the number of occupied days out of the whole year, and it's percentage.
+        ///  The function calculates the percentage of the occupied days out of the whole year.
         /// </summary>
-        /// <param name="Calendar">Calendaris the data base in which the hosting scedule saved inside</param>
+        /// <returns>The percentage of the occupied days out of the whole year.</returns>
         public float GetAnnualBusyPercentage()
         {
             float countOccpiedDays = 0;
@@ -241,13 +171,72 @@ namespace dotNet5780_02_2956_9500
                     }
                 }
             }
-
             return countOccpiedDays / 372;
         }
 
+        /// <summary>
+        /// To eneble comparing btween two hosting units by their amount of occupied days of the year.
+        /// </summary>
+        /// <param name="obj">The other hosting unit object</param>
+        /// <returns>Less than zero	 -This instance precedes obj in the sort order ,Zero - This instance occurs in the same position in the sort order as obj,Greater than zero	- This instance follows obj in the sort order.</returns>
         public int CompareTo(object obj)
         {
             return GetAnnualBusyDays().CompareTo(((HostingUnit)obj).GetAnnualBusyDays());
         }
+
+
+        ///// <summary>
+        /////  The function prints out for each hosting period it's first and last dayof hosting.
+        ///// </summary>
+        ///// <param name="Calendar">Calendaris the data base in which the hosting scedule saved inside</param>
+        //private static string printSchedule(bool[,] Calendar)
+        //{
+        //    string toReturn = string.Empty;
+        //    ArrayList tmpArr = new ArrayList();
+        //    for (int i = 0; i < 12; i++)
+        //    {
+        //        for (int j = 0; j < 31; j++)
+        //        {
+        //            if (Calendar[i, j])
+        //            {
+        //                tmpArr.Add((j + 1) + "/" + (i + 1));
+        //            }
+        //            else
+        //            {
+        //                tmpArr.Add("empty");
+        //            }
+        //        }
+        //    }
+        //    int state = 0;
+        //    for (int i = 0; i < tmpArr.Count; i++)
+        //    {
+        //        if ((i == 0) && (tmpArr[i].ToString() != "empty"))
+        //        {
+        //            toReturn += tmpArr[i].ToString() + " - ";
+        //            // Console.Write(tmpArr[i].ToString() + " - ");
+        //            state++;
+        //        }
+        //        else if ((tmpArr[i].ToString() != "empty") && (state % 2 == 0))
+        //        {
+        //            toReturn += tmpArr[i].ToString() + " - ";
+        //            //Console.Write(tmpArr[i].ToString() + " - ");
+        //            state++;
+        //        }
+        //        else if ((tmpArr[i].ToString() == "empty") && (state % 2 == 1))
+        //        {
+        //            state++;
+        //            toReturn += tmpArr[i - 1].ToString();
+        //            //Console.WriteLine(tmpArr[i - 1].ToString());
+        //        }
+        //    }
+        //    return toReturn;
+        //}
+
+
+
+       
+
+      
+
     }
 }
