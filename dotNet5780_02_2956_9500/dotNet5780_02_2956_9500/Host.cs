f6c040ecf5ea579380efb132b3b73c13f040ecf2
arielsabag <44552742+arielsabag@users.noreply.github.com>
2019-12-06 02:49:02 +0200

    Update Host.cs

--- dotNet5780_02_2956_9500/dotNet5780_02_2956_9500/Host.cs
+++ dotNet5780_02_2956_9500/dotNet5780_02_2956_9500/Host.cs
@@ -14,13 +14,13 @@ namespace dotNet5780_02_2956_9500
         public List<HostingUnit> HostingUnitCollection; // list of hosting units of the Host
 
         /// <summary>
-        /// Ctor
+        /// A constructor function that accepts the host's ID and the amount of hosting units that the Host have, and initializes the HostingUnitCollection list so that all units are free.
         /// </summary>
-        /// <param name="id"></param>
-        /// <param name="hostingUnitAmount"></param>
+        /// <param name="id">Id number of the Host</param>
+        /// <param name="hostingUnitAmount">The amount of hosting units that the host have</param>
         public Host(int id,int hostingUnitAmount)
         {
-            HostKey = id;
+            HostKey = id; 
             HostingUnitCollection = new List<HostingUnit>();
             for (int i = 0; i < hostingUnitAmount; i++) // Initialize the List so that all the units are free
             {
@@ -28,6 +28,10 @@ namespace dotNet5780_02_2956_9500
             }
         }
 
+        /// <summary>
+        /// Displays a list of hosting units of the same host. For each unit: a serial number and the date list in which it is occupied. (the hosting unit's ToString must be used )
+        /// </summary>
+        /// <returns>List of hosting units of the same host. For each unit: a serial number and the date list in which it is occupied.</returns>
         public override string ToString()
         {
             string toReturn = string.Empty;
@@ -38,12 +42,19 @@ namespace dotNet5780_02_2956_9500
             return toReturn;
         }
 
+        /// <summary>
+        /// A function that receives an order request and returns the unit serial number of The hosting unit
+        /// that available at the same host to accept the order. If no such unit exists, the function will return 1.
+        /// - The function will use the ApproveRequest function of a hosting unit.
+        /// </summary>
+        /// <param name="guestReq">The request order</param>
+        /// <returns> If no such unit exists, the function will return 1. otherwise it will return the hosting unit serial key</returns>
         private long SubmitRequest(GuestRequest guestReq)
         {
             bool foundFreeHostingUnit = false;
             int index = 0;
           
-            while ((!foundFreeHostingUnit)&& (index < HostingUnitCollection.Count))
+            while ((!foundFreeHostingUnit) && (index < HostingUnitCollection.Count))
             {
                 foundFreeHostingUnit = HostingUnitCollection.ElementAt(index).ApprovedRequest(guestReq);
                 if (foundFreeHostingUnit)
@@ -54,48 +65,61 @@ namespace dotNet5780_02_2956_9500
             }
             return -1;
         }
+
         /// <summary>
-        ///  The function returns all of occupied days in all hosting units of the host using the function GetHostAnnualBussyDays of the HostingUnits class
+        ///  A function that returns the total number of nights in all of the accommodation units of the owner. 
+        ///  Using the GetAnnualBusyDays hosting unit function.
         /// </summary>
-        /// <returns>Return all of occupied days in all hosting units of the host</returns>
+        /// <returns>the total number of nights in all of the accommodation units of the owner.</returns>
         public int GetHostAnnualBusyDays()
         {
             int sum = 0;
             for (int i = 0; i < HostingUnitCollection.Count; i++)
             {
-                sum += HostingUnitCollection.ElementAt(i).GetAnnualBusyDays();
+                sum += HostingUnitCollection.ElementAt(i).GetAnnualBusyDays();// GetAnnualBusyDays - The function calulates out the number of occupied days out of the whole year. and return it
             }
             return sum;
         }
 
         /// <summary>
-        ///  The function sorts out the HostingUnitsCollection
+        ///  The function sorts out the HostingUnitsCollection according of it's Occupancy.
         /// </summary>
         public void SortUnits()
         {
             HostingUnitCollection.Sort();
         }
-        
+
+        ///// <summary>
+        ///// Function for allocates requests to Hosting Units: The function gets unknown number of GuestRequest and will try to schedule the requests in the order they were accepted in the various Hosting Units order by using the SubmitRequest funtion of the host 
+        ///// </summary>
+        ///// <param name="list"></param>
+        ///// <returns>returns True if all requests were accepted , otherwise returns False  </returns>
+        ///// 
+
+
+
+
         /// <summary>
-        /// Function for allocates requests to Hosting Units: The function gets unknown number of GuestRequest and will try to schedule the requests in the order they were accepted in the various Hosting Units order by using the SubmitRequest funtion of the host 
+        /// Request allocation function for hosting units. The function will get an unknown number of Hosting requirements,
+        /// and try to fix in the requirements in the order in which they were received, In the various hosting units, using the SubmitRequest function of the Host. The function returns true if all requests are possible and false otherwise.
         /// </summary>
-        /// <param name="list"></param>
-        /// <returns>returns True if all requests were accepted , otherwise returns False  </returns>
+        /// <param name="list"> All the guest requests</param>
+        /// <returns>The function returns true if all requests are possible and false otherwise</returns>
         public bool AssignRequests(params GuestRequest[] list)
         {
-            bool available = true;
+            bool notAvailable = false;
             for (int i = 0; i < list.Length; i++)
             {
                 if (SubmitRequest(list[i])==-1)
                 {
-                    available = false;
+                    notAvailable = true;
                 }
             }
-            if (available)
+            if (notAvailable)
             {
-                return true;
+                return false;
             }
-            return false;            
+            return true;            
         }
 
         /// <summary>
@@ -115,9 +139,13 @@ namespace dotNet5780_02_2956_9500
                     }
                 }
                     return null;
-                }           
+             }           
         }
 
+        /// <summary>
+        /// To make it possible to cross the department with a foreach loop, that in any  Iteration  another host unit of the host will return.
+        /// </summary>
+        /// <returns>hosting unit</returns>
         public IEnumerator GetEnumerator()
         {
             foreach (HostingUnit val in HostingUnitCollection)
